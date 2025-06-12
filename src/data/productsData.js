98d8501867bc1813bce62c44cd22ce57860eb66f
Gunawansaputra123 <gunawangsasaputra@gmail.com>
2025-06-12 15:36:30 +0700

    jsulala

--- src/data/productsData.js
+++ src/data/productsData.js
@@ -8,7 +8,7 @@ const productsData = [
   { id: 7, name: 'Risol', category: 'Makanan', price: 5000, description: 'Risol mayo, sayur dan rebung enak murah.', imageUrl: 'https://www.masakapahariini.com/wp-content/uploads/2023/06/risoles-ragout.jpeg' },
   { id: 8, name: 'Sate Ayam', category: 'Makanan', price: 10000, description: 'Sate ayam enak lezat murah berkualitas.', imageUrl: 'https://www.dapurkobe.co.id/wp-content/uploads/sate-ayam.jpg' },
   { id: 9, name: 'Ketoprak', category: 'Makanan', price: 8000, description: 'Ketoprak enak dengan bumbu kacang.', imageUrl: 'https://asset.kompas.com/crops/H84-oywnJbx-F-88tV7RV9xbi4E=/0x0:739x493/1200x800/data/photo/2020/01/29/5e318845429db.jpg' },
-  { id: 9, name: 'Dimsum', category: 'Makanan', price: 4000, description: 'Dimsum enak lembut daging ayam.', imageUrl: 'https://cdn.idntimes.com/content-images/community/2024/06/img-20240605-192130-2b64a83f842f8dac9ad37a9c9fa77858.jpg' },
+  { id: 10, name: 'Dimsum', category: 'Makanan', price: 4000, description: 'Dimsum enak lembut daging ayam.', imageUrl: 'https://cdn.idntimes.com/content-images/community/2024/06/img-20240605-192130-2b64a83f842f8dac9ad37a9c9fa77858.jpg' },
  
   // Tambah produk lain jika perlu
 ];
